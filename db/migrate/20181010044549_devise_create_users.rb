02e37b482737d326ba5d47155e45207655615e2b
hataerin <goil1113@likelion.org>
2018-10-10 14:52:52 +0900

    scaffold/devise

---
+++ db/migrate/20181010044549_devise_create_users.rb
@@ -0,0 +1,63 @@
+# frozen_string_literal: true
+
+class DeviseCreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      ## Database authenticatable
+      t.string :email,              null: false, default: ""
+      t.string :encrypted_password, null: false, default: ""
+
+      ######### add column ################################################
+
+      #이름
+      t.string :user_name,          null: false
+      #생년월일
+      t.date :birthday,             null: false
+      #세션결석횟수
+      t.integer :session_absence,   null: false, default: 0
+      #플젝결석횟수
+      t.integer :project_absence,   null: false, default: 0
+      #지각비
+      t.integer :penalty,           null: false, default: 0
+      #소속프로젝트
+      t.integer :project_id,        null: false
+      #프로필사진
+      t.string :user_photo,         null: false
+
+      #####################################################################
+
+      ## Recoverable
+      t.string   :reset_password_token
+      t.datetime :reset_password_sent_at
+
+      ## Rememberable
+      t.datetime :remember_created_at
+
+      ## Trackable
+      # t.integer  :sign_in_count, default: 0, null: false
+      # t.datetime :current_sign_in_at
+      # t.datetime :last_sign_in_at
+      # t.string   :current_sign_in_ip
+      # t.string   :last_sign_in_ip
+
+      ## Confirmable
+      # t.string   :confirmation_token
+      # t.datetime :confirmed_at
+      # t.datetime :confirmation_sent_at
+      # t.string   :unconfirmed_email # Only if using reconfirmable
+
+      ## Lockable
+      # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
+      # t.string   :unlock_token # Only if unlock strategy is :email or :both
+      # t.datetime :locked_at
+
+
+      t.timestamps null: false
+    end
+
+    add_index :users, :email,                unique: true
+    add_index :users, :reset_password_token, unique: true
+    # add_index :users, :confirmation_token,   unique: true
+    # add_index :users, :unlock_token,         unique: true
+  end
+end
