80adc70214ef2dd2cd73717a4855c2366b5f4f7b
hataerin <goil1113@likelion.org>
2018-10-10 20:56:05 +0900

    fix user signup

--- db/migrate/20181010112706_create_users.rb
+++ db/migrate/20181010112706_create_users.rb
@@ -1,7 +1,5 @@
-# frozen_string_literal: true
-
-class DeviseCreateUsers < ActiveRecord::Migration
-  def change
+class CreateUsers < ActiveRecord::Migration
+ def change
     create_table :users do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181010044549) do
+ActiveRecord::Schema.define(version: 20181010112706) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 20181010044549) do
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
+    t.string   "user_name",                           null: false
+    t.date     "birthday",                            null: false
+    t.integer  "session_absence",        default: 0,  null: false
+    t.integer  "project_absence",        default: 0,  null: false
+    t.integer  "penalty",                default: 0,  null: false
+    t.integer  "project_id",                          null: false
+    t.string   "user_photo",                          null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
